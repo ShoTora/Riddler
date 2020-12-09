3ace16c042d5875ee01b94dfd78632d7b0cf179d
shotaro.kawaguchi <josfas88@yahoo.co.jp>
2020-12-09 14:50:55 +0900

    Initial Commit

---
+++ Riddler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Riddler
+//
+//  Created by ShoTora on 2020/12/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
