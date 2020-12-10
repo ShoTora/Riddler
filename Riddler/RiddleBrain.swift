00c6a23ad7e80e94a1563692bb4521ee9c3e571e
shotaro.kawaguchi <josfas88@yahoo.co.jp>
2020-12-10 17:49:37 +0900

    commit

--- Riddler/RiddleBrain.swift
+++ Riddler/RiddleBrain.swift
@@ -12,19 +12,11 @@ struct RiddleBrain {
     var questionNumber = 0
     
     let quiz = [
-        Question(q: "A slug's blood is green.", a: "True"),
-        Question(q: "Approximately one quarter of human bones are in the feet.", a: "True"),
-        Question(q: "The total surface area of two human lungs is approximately 70 square metres.", a: "True"),
-        Question(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: "True"),
-        Question(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: "False"),
-        Question(q: "It is illegal to pee in the Ocean in Portugal.", a: "True"),
-        Question(q: "You can lead a cow down stairs but not up stairs.", a: "False"),
-        Question(q: "Google was originally called 'Backrub'.", a: "True"),
-        Question(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: "True"),
-        Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: "False"),
-        Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: "False"),
-        Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: "True")
-    ]
+        Question(q: "What can be seen once in a minute, twice in a moment, and never in a thousand years?", a: "The Letter “M”"),
+        Question(q: "which word is written incorrectly in a dictionary?", a: "The Word “Incorrectly”"),
+        Question(q: "What flies when it’s born, lies when it’s alive, and runs when it’s dead?", a: "A Snowflake"),
+        Question(q: "I’m always somewhere between the ground and the sky, always far in the distance, always moving further away if one attempts to come closer. What am I?", a: "The Horizon"),
+        ]
     
     func getQuestionText() -> String {
         return quiz[questionNumber].text
